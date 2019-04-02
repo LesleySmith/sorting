576b10fa7797cefcf00940fb7cb3e882f5560557
LesleySmith <lesleysmith281@gmail.com>
2019-04-02 11:36:54 -0500

    added spyon

--- boublesort.js
+++ boublesort.js
@@ -3,7 +3,7 @@ function bubbleSort(array) {
         for (i=1; i<j; i++){
             if (array[i]<array[i-1]){
                 swap(array, i-1, i)
-            } 
+            }
         }
     }
     return array
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,18 +1,21 @@
 describe('Bubble Sort', function(){
+  beforeAll(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
     it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
+      expect( window.swap.calls.count() ).toEqual( 0 );
     });
     it('handles an array of one item', function(){
         expect( bubbleSort([69]) ).toEqual( [69] );
+        expect( window.swap.calls.count() ).toEqual( 0 );
     })
     it('sorts array members in numberical order', function(){
         expect( bubbleSort([12,23,45,6,8,82,42]) ).toEqual( [6,8,12,23,42,45,82] );
+        expect( window.swap.calls.count() ).toEqual( 8 );
     })
   });
-
-
-
-
 
 
 
